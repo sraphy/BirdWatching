c1c3c8dec76942633490baed52e993d904faa133
sraphy <sraphy506@gmail.com>
2014-07-07 18:10:43 +0800

    Changes not staged for commit: 	modified:   BirdWatching.xcodeproj/project.pbxproj 	modified:   BirdWatching/AddSightingViewController.h 	modified:   BirdWatching/AddSightingViewController.m 	modified:   BirdWatching/Base.lproj/Main.storyboard  Untracked files: 	BirdWatching.xcodeproj/project.xcworkspace/xcshareddata/

---
+++ BirdWatching/LocationManagerController.h
@@ -0,0 +1,13 @@
+//
+//  LocationManagerController.h
+//  BirdWatching
+//
+//  Created by sraphy on 7/7/14.
+//  Copyright (c) 2014 TianYu Tech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LocationManagerController : NSObject
+
+@end
---
+++ BirdWatching/LocationManagerController.m
@@ -0,0 +1,13 @@
+//
+//  LocationManagerController.m
+//  BirdWatching
+//
+//  Created by sraphy on 7/7/14.
+//  Copyright (c) 2014 TianYu Tech. All rights reserved.
+//
+
+#import "LocationManagerController.h"
+
+@implementation LocationManagerController
+
+@end
